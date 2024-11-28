88bdc6095e560192516acfee50e8735d7e8a7d3c
yas <yas@gmail.com>
2024-11-28 22:05:40 +0100

    Refactor gameGrid

--- src/component/GameGrid.tsx
+++ src/component/GameGrid.tsx
@@ -1,46 +1,41 @@
 import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 
-
 import useGames, { Platform } from "../hooks/useGames";
 import { Genre } from "../hooks/useGenres";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
-import {GameQuery} from '../App'
+import { GameQuery } from "../App";
 
-
-interface Props{
-  gameQuery:GameQuery;
- 
+interface Props {
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({gameQuery}: Props) => {
+const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6]; // we use it to iterat over because we dont have any data specifay the number of skeletons , we 6 items
 
+  if (error) return <Text>{error}</Text>;
+  // to avoit <></>  {error && <Text>{error} </Text>} in return to componeet
   return (
-    <>
-      {error && <Text>{error} </Text>}
-
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-        padding="10px"
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton  />
-            </GameCardContainer>
-          ))}
-
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-              <GameCard  game={game} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+      padding="10px"
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
